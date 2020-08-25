644a3e96c7a1a1a56b2fd6df6a85d18493c38b79
faaat <zjf580@163.com>
2020-08-25 10:51:51 +0800

    debug report cron

--- public/crons/daily_report.php
+++ public/crons/daily_report.php
@@ -6,6 +6,7 @@ require_once __DIR__ . '/../init.inc.php';
 $startDate = isset($argv[1]) ? strtotime($argv[1]) : strtotime('-2 day');
 $endDate =  max(strtotime('-30 day'), strtotime('20200815'));
 
+var_dump(date('Y-m-d',$startDate));
 
 // 获取巨量引擎推广数据
 $topon = new \Core\Topon();
@@ -27,8 +28,8 @@ while (true) {
                 $sql = 'UPDATE r_report SET ' . $roi . '  = ?, new_user_topon = ?, new_user_ocean = ? WHERE report_date = ? AND ad_id = ?';
                 $locator->db->exec($sql, $roiVal, $toponInfo['new_user'], $reportInfo['convert'], $reportDate, $reportInfo['ad_id']);
             } else {
-                $sql = 'INSERT INTO r_report SET report_date = ?, advertiser_id = ?, campaign_id = ?, ad_id = ?, ' . $roi . ' = ?, new_user_topon = ?, new_user_ocean = ?';
-                $locator->db->exec($sql, $reportDate, $reportInfo['advertiser_id'], $reportInfo['campaign_id'], $reportInfo['ad_id'], $roiVal, $toponInfo['new_user'], $reportInfo['convert']);
+                $sql = 'INSERT INTO r_report SET report_date = ?, advertiser_id = ?, campaign_id = ?, ad_id = ?, ' . $roi . ' = ?, new_user_topon = ?, new_user_ocean = ?, new_user_gap = ? ';
+                $locator->db->exec($sql, $reportDate, $reportInfo['advertiser_id'], $reportInfo['campaign_id'], $reportInfo['ad_id'], $roiVal, $toponInfo['new_user'], $reportInfo['convert'],  $reportInfo['convert'] ? ROUND(1 - $toponInfo['new_user'] / $reportInfo['convert'], 4) : 0);
             }
         }
     }
